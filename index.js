8f34809a6a9cdb6cc5801dc1941fd3dffda593eb
valeria campilongo <cvaleriac@hotmail.it>
2019-06-07 17:29:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function BoardMember(name, homeState, training) {
 
   this.veto = function(){
     console.log("No, I must disagree!");
-  };
+  }
   this.approve = function(){
     console.log("You can do that!");
-  };
+  }
   this.charity = function(){
     console.log("I like to help people");
-  };
+  }
   this.releasePressStatement = function(){
     console.log("You will see great things from Scuber.");
   };
