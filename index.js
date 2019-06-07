10ed687c967d9eac7125b28f2959403f9fa92961
valeria campilongo <cvaleriac@hotmail.it>
2019-06-07 17:36:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ function BoardMember(name, homeState, training) {
   this.training = training;
 
   this.veto = function(){
-    console.log("No, I must disagree!");
-  };
+    return "No, I must disagree!";
+  }
   this.approve = function(){
     console.log("You can do that!");
   }
   this.doCharity = function(){
-    console.log("I like to help people");
+    return "I like to help people";
   }
   this.releasePressStatement = function(){
-    console.log("You will see great things from Scuber.");
+    return "You will see great things from Scuber.";
   };
   this.sayHi = function(){
-    console.log("Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.city}");
+    return "Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}";
   }
 }
